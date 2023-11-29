66a12bac8f6261fef161b7617206659faadaae7c
Prasadpbk <129621326+Prasad1eeeee@users.noreply.github.com>
2023-11-29 09:20:15 +0530

    now

--- Routes/Createuser.js
+++ Routes/Createuser.js
@@ -2,30 +2,44 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const mongodb = require('../db'); // Require mongodb
+const { body, validationResult } = require('express-validator');
 
-router.post('/createuser', async (req, res) => {
-    try {
-        // Wait for the MongoDB connection to be ready
-        await mongodb();
+router.post('/createuser', [
 
-        // Create a new user document
-        const newUser = new User({
-            name: req.body.name,
-            location: req.body.location,
-            email: req.body.email,
-            password: req.body.password
-        });
+    body('email').isEmail(),
+    body('name').isLength({ min: 5 }),
+    body('password','password is not strong').isLength({ min: 5 })]
 
-        // Save the new user document to the database
-        await newUser.save();
+    , async (req, res) => {
 
-        // Send a success response
-        res.json({ success: true });
-    } catch (error) {
-        // Handle any errors
-        console.error(error);
-        res.status(500).json({ success: false });
-    }
-});
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            // Wait for the MongoDB connection to be ready
+            await mongodb();
+
+            // Create a new user document
+            const newUser = new User({
+                name: req.body.name,
+                location: req.body.location,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            // Save the new user document to the database
+            await newUser.save();
+
+            // Send a success response
+            res.json({ success: true });
+        } catch (error) {
+            // Handle any errors
+            console.error(error);
+            res.status(500).json({ success: false });
+        }
+    });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 5000;
 const mongodb = require('./db'); // Adjust the path as needed
 
 // Call the mongodb function to connect to MongoDB
+
+app.use((req,res,next)=>{
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+
+})
 mongodb();
 
 app.get('/', (req, res) => {
